ede724597670be1b7d42bd3797ba510a54917015
fridasanchez427-prog <frida.sanchez427@gmail.com>
2026-07-22 11:26:06 -0700

    changes

---
+++ Assets/[Scripts]/main menu controler.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class mainmenucontroler : MonoBehaviour
+{
+    public void PlayGame()
+    {
+       //SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    /*public void QuitGame()
+    {
+        Debug.Log("Quit application triggered."); 
+        Application.Quit(); 
+    }*/
+}
